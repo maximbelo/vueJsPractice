7d36ab4ef8fb0e948cac5150a04e0f96391c2cc3
maximbelo <mbelomestnov@hotmail.ca>
2022-02-16 14:44:41 -0500

    Rebuilt demo app with Vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,34 @@
-// Variables
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+// ===========================================
+// Vanilla JS
+// ===========================================
 
-buttonEl.addEventListener("click", () => {
-  const enteredVal = inputEl.value;
-  const listItemEl = document.createElement("li");
-  listItemEl.textContent = enteredVal;
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-  listEl.append(listItemEl);
-  inputEl.value = "";
-});
+// buttonEl.addEventListener("click", () => {
+//   const enteredVal = inputEl.value;
+//   const listItemEl = document.createElement("li");
+//   listItemEl.textContent = enteredVal;
+
+//   listEl.append(listItemEl);
+//   inputEl.value = "";
+// });
+
+// ===========================================
+// Vue JS
+// ===========================================
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
