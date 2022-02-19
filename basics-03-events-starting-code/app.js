20850ce12c7ba8e37d46627c207bf4bdb0462386
maximbelo <mbelomestnov@hotmail.ca>
2022-02-19 11:24:00 -0500

    Working with multiple event modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,12 +3,22 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      confirmedName: "",
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+
+    submitForm() {
+      alert("Submitted!");
+    },
+
     setName(event, lastName) {
       this.name = event.target.value + " " + lastName;
     },
+
     addNum(num) {
       this.counter = this.counter + num;
     },
