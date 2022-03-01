d564b4e102d7db56593ae270034369e315f198d6
maximbelo <mbelomestnov@hotmail.ca>
2022-02-28 21:49:34 -0500

    Conditional rendering practice

---
+++ lists-cond-01-starting-setup/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredGoalValue: "",
+    };
+  },
+
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
+  },
+});
+
+app.mount("#user-goals");
